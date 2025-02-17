9c7515090bed9ba64b237f61b59d40ba18f3d5db
Sabareesh-SS <sabareesh3898@gmail.com>
2025-02-17 11:14:20 +0530

    updated

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,17 +4,17 @@ import "./Navbar.css"
 function Navbar() {
   return (
     <nav>
-      <a href="/lazyloading">Home</a>
-      <a href="/lezyloading/Page1">Page 1</a>
-      <a href="/lezyloading/Page2">Page 2</a>
-      <a href="/lezyloading/Page3">Page 3</a>
-      <a href="/lezyloading/Page4">Page 4</a>
-      <a href="/lezyloading/Page5">Page 5</a>
-      <a href="/lezyloading/Page6">Page 6</a>
-      <a href="/lezyloading/Page7">Page 7</a>
-      <a href="/lezyloading/Page8">Page 8</a>
-      <a href="/lezyloading/Page9">Page 9</a>
-      <a href="/lezyloading/Page10">Page 10</a>
+      <a href="/">Home</a>
+      <a href="/Page1">Page 1</a>
+      <a href="/Page2">Page 2</a>
+      <a href="/Page3">Page 3</a>
+      <a href="/Page4">Page 4</a>
+      <a href="/Page5">Page 5</a>
+      <a href="/Page6">Page 6</a>
+      <a href="/Page7">Page 7</a>
+      <a href="/Page8">Page 8</a>
+      <a href="/Page9">Page 9</a>
+      <a href="/Page10">Page 10</a>
     </nav>
   )
 }
